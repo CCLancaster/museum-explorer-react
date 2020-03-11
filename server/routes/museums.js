d1fa25dd7da4898ba4e88d91cabdb289be5dc073
CCLancaster <connieclancaster@gmail.com>
2020-03-11 16:59:22 -0700

    finished code along wtih Sarah, will need to complete routes for hw tonight

--- server/routes/museums.js
+++ server/routes/museums.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router()
+const db = require('../models/index');
 
+router.get('/', (req, res) => {
+    db.Museum.find()
+    .then(museums => {
+        res.send(museums);
+    }).catch(err => res.send({ message: 'Error in getting all museaums', err}));
+})
 
+router.get('/:id', (req, res) => {
+    db.Museum.findById(req.params.id)
+    .then(museum=>res.send(museum))
+    .catch(err=>res.send({ message: 'Error in getting one  museum', err}));
+})
 module.exports = router;
