f180dbaa7bff144f671a49c032864ada78ae80ac
CCLancaster <connieclancaster@gmail.com>
2020-03-12 07:31:18 -0700

    added get routes to pieces

--- server/routes/pieces.js
+++ server/routes/pieces.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router()
+const db = require('../models/index');
 
+router.get('/', (req, res) => {
+    db.Piece.find()
+    .then(pieces => {
+        res.send(pieces);
+    }).catch(err => res.send({ message: 'Error in getting all pieces', err}));
+})
 
+router.get('/:id', (req, res) => {
+    db.Piece.findById(req.params.id)
+    .then(piece=>res.send(piece))
+    .catch(err=>res.send({ message: 'Error in getting one piece', err}));
+})
 module.exports = router;
